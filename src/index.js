c3ea31e3ab9eedf8aeea7ad09af03f19c2bb407e
racie23 <132456609+racie23@users.noreply.github.com>
2026-04-14 22:54:11 +0000

    Injected the forecast from js

--- src/index.js
+++ src/index.js
@@ -54,5 +54,33 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+  <div class="weather-forecast-day">
+    <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌥️</div>
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>28º </strong>
+      </div>
+      <div class="weather-forecast-temperature">9º</div>
+    </div>
+  </div>
+`;
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
+displayForecast();
